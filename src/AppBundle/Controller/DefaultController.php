993f9c3f13259c28e3f1798753f6e02274f42614
Abdelfattah <abdelfattah59@gmail.com>
2020-12-21 17:58:04 +0100

    Frontend page blog and show blog

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,10 +17,37 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         $em = $this->getDoctrine()->getManager();
 
-        $posts = $em->getRepository('AdminBundle:Post')->findBy([], ['datepublish'=>'DESC'], 3, null);
+        $posts = $em->getRepository('AdminBundle:Post')->findBy([], ['datepublish' => 'DESC'], 3, null);
         return $this->render('default/index.html.twig', [
             'posts' => $posts
         ]);
     }
-    
+
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blog()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $posts = $em->getRepository('AdminBundle:Post')->findAll();
+        return $this->render('default/blog.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+     /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $post = $em->getRepository('AdminBundle:Post')->find($id);
+        return $this->render('default/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+   
 }
